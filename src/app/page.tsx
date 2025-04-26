4c8eec96832a01d501469fc5a977369388c805a9
Kostiantyn Ochenash <Ochenashk@gmail.com>
2025-04-26 19:47:09 +0200

    deploy first MVP

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+// src\app\page.tsx
+const WelCUMeHomeSweetheart = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <h1 className="text-4xl font-bold"> Draksa!</h1>
+
+      <h2 className="text-4xl font-bold"> WelCUMe Home, sweetheart!</h2>
+
+      <p className="mt-4 text-lg"> Get smarter first, you jerk :D</p>
+    </main>
+  );
+};
+
+export default WelCUMeHomeSweetheart;
