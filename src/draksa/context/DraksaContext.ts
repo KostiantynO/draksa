528bfcad35f66fe26f1e11a4738b62eac0f3a996
Kostiantyn Ochenash <Ochenashk@gmail.com>
2026-02-16 22:40:01 +0100

    refactor: :fire: remove context

--- src/draksa/context/DraksaContext.ts
+++
@@ -1,8 +0,0 @@
-// src\draksa\context\DraksaContext.ts
-'use client';
-
-import { createContext } from 'react';
-
-import type { DraksaContextType } from '../mood/mood';
-
-export const DraksaContext = createContext<DraksaContextType | undefined>(undefined);
