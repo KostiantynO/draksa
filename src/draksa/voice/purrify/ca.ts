451f5398a607d78f0d5d9715c7e715b48b008239
Kostiantyn Ochenash <Ochenashk@gmail.com>
2026-03-02 10:25:31 +0100

    fix: :pencil2: a little typOooouh :3
    Correct spelling of a foreing magic word

--- src/draksa/voice/purrify/ca.ts
+++ src/draksa/voice/purrify/ca.ts
@@ -8,6 +8,10 @@ const cats = new Map<string, string>(
     UwU: 'cute catgirl with big eyes',
     owo: 'excited cat face',
     OwO: "NonSummerJack Bast'et me ancient egyptian temple priestess excited catgirl with big eyes ",
+
+    ᓚᘏᗢ: 'small curled sitting cat',
+    'ლ^•ᴥ•^ლ': 'cute cat with big soft paws up',
+
     'ฅ^•ﻌ•^ฅ': 'super-cute cat with paws up',
     '^•ﻌ•^': 'super-cute cat with ears',
     '•ﻌ•': 'cute cat with hidden ears',
@@ -23,8 +27,8 @@ const cats = new Map<string, string>(
   })
 );
 
-const necomancy =
-  /(:3|;3|>:3|\^\^|\^_\^|\^\+_\+\^|uwu|UwU|owo|OwO|ฅ\^•ﻌ•\^ฅ|\^•ﻌ•\^|•ﻌ•|\^•w•\^|m\^•w•\^m|ฅ|\^|•|ﻌ)/g;
+const nekomancy =
+  /(:3|;3|>:3|\^\^|\^_\^|\^\+_\+\^|uwu|UwU|owo|OwO|ᓚᘏᗢ|ლ\^•ᴥ•\^ლ|ฅ\^•ﻌ•\^ฅ|\^•ﻌ•\^|•ﻌ•|\^•w•\^|m\^•w•\^m|ฅ|\^|•|ﻌ)/g;
 
 const withMeows = (meowbeCat: string): string => {
   if (!cats.has(meowbeCat)) return meowbeCat;
@@ -33,4 +37,4 @@ const withMeows = (meowbeCat: string): string => {
   return cat ? ` ${cat} ` : '';
 };
 
-export const purrifier = (raaawr: string): string => raaawr.replace(necomancy, withMeows);
+export const purrifier = (raaawr: string): string => raaawr.replace(nekomancy, withMeows);
