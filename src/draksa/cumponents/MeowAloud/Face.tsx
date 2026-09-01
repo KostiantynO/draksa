46166e60867ebf6ecc9a0876e2991d65e0cf432e
Kostiantyn Ochenash <Ochenashk@gmail.com>
2026-09-01 07:04:56 +0200

    feat: :sparkles: Draksa😻🐉🐍: I'm so hungry!
    Heloїsa: Go after her, Honey. Make sure she doesn't get in trouble and give her some small slice of pork :)

--- src/draksa/cumponents/MeowAloud/Face.tsx
+++ src/draksa/cumponents/MeowAloud/Face.tsx
@@ -1,7 +1,21 @@
 // src\draksa\cumponents\MeowAloud\Face.tsx
+'use client';
+
+import Image from 'next/image';
+
+import family from '@/draksa/art/family.webp';
 
 import type { ReactNode } from 'react';
 
 export const Face = ({ children }: { children: ReactNode }) => {
-  return <div className="flex flex-col gap-2 px-4">{children}</div>;
+  return (
+    <div>
+      <Image
+        src={family}
+        alt="Draksa family"
+        className="absolute -z-10 bg-linear-21 from-indigo-500 via-purple-500 to-pink-500"
+      />
+      {children}
+    </div>
+  );
 };
--- src/draksa/cumponents/MeowAloud/draksaTells.ts
+++ src/draksa/cumponents/MeowAloud/draksaTells.ts
@@ -1,6 +1,24 @@
 // src\draksa\cumponents\MeowAloud\draksaTells.ts
 
 export const draksaTells = {
-  pleaseFeedMe: 'Please, feed me ...',
+  pleaseFeedMe: `Draksa😻🐉🐍: I'm so hungry! I will go to the paddocks to find me some piggy!
+
+Heloïse🧝‍♀️: Darling, you know you cannot eat them :D
+
+Draksa: Daaaddy, doo you love me? 83
+
+Ashmedai🐲: Of course I love you, Sunshine :D
+
+Draksa: Will you help me to eat some piggy!? :D Pleeease :D
+
+Ashmedai: Sweetheart, you better eat norm...
+
+Draksa: *jumps and hugs* THANK YOU, Daaaddy! Love you! :D Catch me at the pens yard! :D
+
+Ashmedai: Ahh, what to do with her 🤦‍♂️?...
+
+Heloїsa: Go after her, Honey. Make sure she doesn't get in trouble and give her some small slice of pork :)
+
+  `,
   aStory: 'when 🐉 and 🧝‍♀️ love each other, then naga Draksa 😻🐉🐍 is born :D',
 } as const;
